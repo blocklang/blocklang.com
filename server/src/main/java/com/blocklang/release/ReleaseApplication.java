9d9f3e2d4f6fc66c28fd629c8c232f4c99e9cb0c
xiaohulu <idocument@qq.com>
2019-01-03 10:42:41 +0800

    创建 spring boot 和 dojo 项目

---
+++ server/src/main/java/com/blocklang/release/ReleaseApplication.java
@@ -0,0 +1,14 @@
+package com.blocklang.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseApplication.class, args);
+	}
+
+}
+
