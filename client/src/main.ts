fe18d14a8583d55edcc85211a7666da166979c58
xiaohulu <idocument@qq.com>
2019-03-01 12:03:14 +0800

    解决 main.css 的全局样式被 bootstrap 的样式覆盖的问题

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,11 @@
 import renderer from '@dojo/framework/widget-core/vdom';
-import Registry from '@dojo/framework/widget-core/Registry';
 import { w } from '@dojo/framework/widget-core/d';
 import { Store } from '@dojo/framework/stores/Store';
 import { StateHistory } from '@dojo/framework/routing/history/StateHistory';
 import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
 import { registerThemeInjector } from '@dojo/framework/widget-core/mixins/Themed';
 import { registerStoreInjector } from '@dojo/framework/stores/StoreInjector';
+
 import dojo from '@dojo/themes/dojo';
 import '@dojo/themes/dojo/index.css';
 
@@ -18,11 +18,10 @@ import { changeRouteProcess } from './processes/routeProcesses';
 import { initPrivateHomeProcess } from './processes/homeProcesses';
 
 const store = new Store<State>();
-const registry = new Registry();
 
 getCurrentUserProcess(store)({});
 
-registerStoreInjector(store, { registry });
+const registry = registerStoreInjector(store);
 const router = registerRouterInjector(routes, registry, { HistoryManager: StateHistory });
 registerThemeInjector(dojo, registry);
 
