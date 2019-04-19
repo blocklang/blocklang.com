1db99b4d2581a3ed80a8b03dbdbcfe4eaa88bc02
xiaohulu <idocument@qq.com>
2019-04-19 12:00:40 +0800

    fetch 请求中增加 X-Token header

--- client/src/processes/utils.ts
+++ client/src/processes/utils.ts
@@ -1,3 +1,4 @@
+import global from '@dojo/framework/shim/global';
 import { createCommandFactory } from '@dojo/framework/stores/process';
 import { State } from '../interfaces';
 
@@ -5,6 +6,12 @@ export function getHeaders(): any {
 	const headers: { [key: string]: string } = {
 		'X-Requested-With': 'FetchApi'
 	};
+
+	const userSession = global.sessionStorage.getItem('blocklang-session');
+	if (userSession) {
+		headers['X-Token'] = JSON.parse(userSession).token;
+	}
+
 	return headers;
 }
 
