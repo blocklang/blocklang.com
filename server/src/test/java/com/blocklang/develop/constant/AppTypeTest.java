0f6bf39c33a26caf5978c1c30b12dc41ae1206a6
xiaohulu <idocument@qq.com>
2019-05-02 06:04:45 +0800

    修复失败的测试用例

--- server/src/test/java/com/blocklang/develop/constant/AppTypeTest.java
+++ server/src/test/java/com/blocklang/develop/constant/AppTypeTest.java
@@ -7,11 +7,12 @@ public class AppTypeTest {
 
 	@Test
 	public void from_key() {
-		assertThat(AppType.fromKey("01").getValue()).isEqualTo("Web");
+		assertThat(AppType.fromKey("01").getValue()).isEqualTo("web");
 	}
 	
 	@Test
 	public void from_value() {
+		assertThat(AppType.fromValue("web").getKey()).isEqualTo("01");
 		assertThat(AppType.fromValue("Web").getKey()).isEqualTo("01");
 	}
 	
