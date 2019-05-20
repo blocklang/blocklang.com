112c89fc6b9262790bdc6303d079a74dbbd743cf
yzuwangfeng <315512657@qq.com>
2019-05-20 20:43:24 +0800

    完善

--- server/src/main/java/com/blocklang/BlockLangApplication.java
+++ server/src/main/java/com/blocklang/BlockLangApplication.java
@@ -2,25 +2,11 @@ package com.blocklang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @EnableCaching // 启用缓存机制
 @SpringBootApplication
 public class BlockLangApplication {
-
-	@Bean
-	public HandlerExceptionResolver sentryExceptionResolver() {
-		return new io.sentry.spring.SentryExceptionResolver();
-	}
-
-	@Bean
-	public ServletContextInitializer sentryServletContextInitializer() {
-		return new io.sentry.spring.SentryServletContextInitializer();
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlockLangApplication.class, args);
