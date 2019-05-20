31e5c67bcdf4f8f600e4d7fbf3f0d253bfb608e3
xiaohulu <idocument@qq.com>
2019-05-20 21:55:03 +0800

    使用统一的缩进格式，行首使用 tab 键

--- server/src/main/java/com/blocklang/config/SentryConfig.java
+++ server/src/main/java/com/blocklang/config/SentryConfig.java
@@ -1,51 +1,58 @@
 package com.blocklang.config;
 
-import com.blocklang.core.exception.InvalidRequestException;
-import com.blocklang.core.exception.NoAuthorizationException;
-import com.blocklang.core.exception.ResourceNotFoundException;
-import io.sentry.spring.SentryExceptionResolver;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Objects;
+import com.blocklang.core.exception.InvalidRequestException;
+import com.blocklang.core.exception.NoAuthorizationException;
+import com.blocklang.core.exception.ResourceNotFoundException;
+
+import io.sentry.spring.SentryExceptionResolver;
+import io.sentry.spring.SentryServletContextInitializer;
 
 @Configuration
 public class SentryConfig {
-    private Class<?>[] ignoredExceptions = { InvalidRequestException.class, ResourceNotFoundException.class, NoAuthorizationException.class };
 
-    @Bean
-    public HandlerExceptionResolver sentryExceptionResolver() {
-        return new FilterSentryExceptionResolver();
-    }
+	private Class<?>[] ignoredExceptions = { 
+		InvalidRequestException.class,
+		ResourceNotFoundException.class,
+		NoAuthorizationException.class
+	};
 
-    @Bean
-    public ServletContextInitializer sentryServletContextInitializer() {
-        return new io.sentry.spring.SentryServletContextInitializer();
-    }
+	@Bean
+	public HandlerExceptionResolver sentryExceptionResolver() {
+		return new FilterSentryExceptionResolver();
+	}
 
-    public class FilterSentryExceptionResolver extends SentryExceptionResolver{
-        @Override
-        public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-            if(ex != null){
-                if(needReport(ex)){
-                    return super.resolveException(request, response, handler, ex);
-                }
-            }
-            return null;
-        }
+	@Bean
+	public ServletContextInitializer sentryServletContextInitializer() {
+		return new SentryServletContextInitializer();
+	}
 
-        private boolean needReport(Exception e){
-            for(Class<?> clazz : ignoredExceptions){
-                if(!Objects.isNull(clazz) && clazz.isAssignableFrom(e.getClass())){
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+	class FilterSentryExceptionResolver extends SentryExceptionResolver {
+
+		@Override
+		public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+			if(e == null) {
+				return null;
+			}
+			if(ignoreReport(e)) {
+				return null;
+			}
+			return super.resolveException(request, response, handler, e);
+		}
+
+		private boolean ignoreReport(Exception e) {
+			return Arrays.stream(ignoredExceptions).anyMatch(item -> item.isAssignableFrom(e.getClass()));
+		}
+	}
 }
