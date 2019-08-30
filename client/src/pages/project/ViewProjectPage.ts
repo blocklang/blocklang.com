8417517020b6557a7a999f7498f34a315b279baa
xiaohulu <idocument@qq.com>
2019-08-30 15:36:29 +0800

    引用 PageDesigner 部件

--- client/src/pages/project/ViewProjectPage.ts
+++ client/src/pages/project/ViewProjectPage.ts
@@ -2,8 +2,9 @@ import * as css from './ViewProject.m.css';
 import ThemedMixin, { theme } from '@dojo/framework/core/mixins/Themed';
 import I18nMixin from '@dojo/framework/core/mixins/I18n';
 import WidgetBase from '@dojo/framework/core/WidgetBase';
-import { v } from '@dojo/framework/core/vdom';
+import { v, w } from '@dojo/framework/core/vdom';
 import * as c from '../../className';
+import PageDesigner from 'page-designer/PageDesigner';
 
 export interface ViewProjectPageProperties {}
 
@@ -12,6 +13,6 @@ export default class ViewProjectPage extends ThemedMixin(I18nMixin(WidgetBase))<
 	//private _localizedMessages = this.localizeBundle(messageBundle);
 
 	protected render() {
-		return v('div', { classes: [c.text_center, c.mt_5] }, [v('h1', ['项目 - 页面'])]);
+		return v('div', { classes: [c.text_center, c.mt_5] }, [v('h1', ['项目 - 页面']), w(PageDesigner, {})]);
 	}
 }
