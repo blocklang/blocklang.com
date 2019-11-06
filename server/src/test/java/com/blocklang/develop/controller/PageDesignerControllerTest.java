f78732c2139bff3f8d992cde86f98ddf7ed60a2e
xiaohulu <idocument@qq.com>
2019-11-06 16:18:27 +0800

    将获取项目依赖的部件列表的测试用例移到 PageDesigner 相关类中

--- server/src/test/java/com/blocklang/develop/controller/PageDesignerControllerTest.java
+++ server/src/test/java/com/blocklang/develop/controller/PageDesignerControllerTest.java
@@ -3,6 +3,7 @@ package com.blocklang.develop.controller;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
@@ -12,10 +13,14 @@ import org.apache.http.HttpStatus;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 
+import com.blocklang.core.model.UserInfo;
 import com.blocklang.core.test.AbstractControllerTest;
+import com.blocklang.develop.constant.AccessLevel;
 import com.blocklang.develop.data.ProjectDependenceData;
 import com.blocklang.develop.model.Project;
+import com.blocklang.develop.model.ProjectAuthorization;
 import com.blocklang.develop.model.ProjectDependence;
 import com.blocklang.develop.service.ProjectAuthorizationService;
 import com.blocklang.develop.service.ProjectDependenceService;
@@ -141,5 +146,124 @@ public class PageDesignerControllerTest extends AbstractControllerTest {
 					"[0].category", equalTo("Widget"),
 					"[0].version", equalTo("0.0.1"),
 					"[0].std", equalTo(true));
+	}
+	
+	@Test
+	public void get_project_dependeces_widgets_project_not_found() {
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.empty());
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_NOT_FOUND)
+			.body(equalTo(""));
+	}
+	
+	// 匿名用户不能访问私有项目
+	@Test
+	public void get_project_dependeces_widgets_anonymous_user_forbidden_access_private_project() {
+		Project project = new Project();
+		project.setId(1);
+		project.setIsPublic(false);
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.of(project));
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_FORBIDDEN)
+			.body(equalTo(""));
+	}
+	
+	// 匿名用户能访问公开项目
+	@Test
+	public void get_project_dependeces_widgets_anonymous_user_can_access_public_project() {
+		Project project = new Project();
+		project.setId(1);
+		project.setIsPublic(true);
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.of(project));
+		
+		when(projectDependenceService.findAllWidgets(anyInt())).thenReturn(Collections.emptyList());
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_OK)
+			.body(equalTo("[]"));
+	}
+
+	// 登录用户能访问公开项目
+	@WithMockUser("jack")
+	@Test
+	public void get_project_dependeces_widgets_login_user_can_access_public_project() {
+		Project project = new Project();
+		project.setId(1);
+		project.setIsPublic(true);
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.of(project));
+		
+		when(projectDependenceService.findAllWidgets(anyInt())).thenReturn(Collections.emptyList());
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_OK)
+			.body(equalTo("[]"));
+	}
+
+	// 登录用户不能访问没有 read | write | admin 权限的私有项目
+	@WithMockUser("jack")
+	@Test
+	public void get_project_dependeces_widgets_login_user_can_not_access_private_project_that_has_no_permission() {
+		Project project = new Project();
+		project.setId(1);
+		project.setIsPublic(false);
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.of(project));
+		
+		UserInfo user = new UserInfo();
+		user.setId(1);
+		when(userService.findByLoginName(anyString())).thenReturn(Optional.of(user));
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_FORBIDDEN)
+			.body(equalTo(""));
+	}
+	
+	// 登录用户能访问有 read | write | admin 权限的私有项目
+	@WithMockUser("jack")
+	@Test
+	public void get_project_dependeces_widgets_login_user_can_not_access_private_project_that_has_read_permission() {
+		Project project = new Project();
+		project.setId(1);
+		project.setIsPublic(false);
+		when(projectService.find(anyString(), anyString())).thenReturn(Optional.of(project));
+		
+		UserInfo user = new UserInfo();
+		user.setId(1);
+		when(userService.findByLoginName(anyString())).thenReturn(Optional.of(user));
+		
+		ProjectAuthorization auth = new ProjectAuthorization();
+		auth.setUserId(1);
+		auth.setProjectId(1);
+		auth.setAccessLevel(AccessLevel.READ);
+		when(projectAuthorizationService.findAllByUserIdAndProjectId(anyInt(), anyInt())).thenReturn(Collections.singletonList(auth));
+		
+		given()
+			.contentType(ContentType.JSON)
+		.when()
+			.get("/projects/{owner}/{projectName}/dependences/widgets", "jack", "project")
+		.then()
+			.statusCode(HttpStatus.SC_OK)
+			.body(equalTo("[]"));
 	}
 }
