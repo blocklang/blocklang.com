a23b56158e55345bc00221b3a522063ba4d7e70b
xiaohulu <idocument@qq.com>
2019-11-16 09:32:39 +0800

    添加返回到父分组事件

--- client/src/containers/project/ViewProjectPageContainer.ts
+++ client/src/containers/project/ViewProjectPageContainer.ts
@@ -2,6 +2,7 @@ import Store from '@dojo/framework/stores/Store';
 import { State } from '../../interfaces';
 import { StoreContainer } from '@dojo/framework/stores/StoreInjector';
 import ViewProjectPage, { ViewProjectPageProperties } from '../../pages/project/ViewProjectPage';
+import { initForViewProjectGroupProcess } from '../../processes/projectProcesses';
 
 function getProperties(store: Store<State>): ViewProjectPageProperties {
 	const { get, path } = store;
@@ -10,7 +11,8 @@ function getProperties(store: Store<State>): ViewProjectPageProperties {
 		loginUser: get(path('user')),
 		project: get(path('project')),
 		resource: get(path('projectResource')),
-		groups: get(path('parentGroups'))
+		groups: get(path('parentGroups')),
+		onGotoGroup: initForViewProjectGroupProcess(store)
 	};
 }
 
--- client/src/pages/project/ViewProjectPage.ts
+++ client/src/pages/project/ViewProjectPage.ts
@@ -7,12 +7,15 @@ import PageDesigner from 'page-designer/PageDesigner';
 import { User, Project, ProjectResource, ProjectResourceGroup } from '../../interfaces';
 import { canReadPage, canEditPage } from '../../permission';
 import Spinner from '../../widgets/spinner';
+import global from '@dojo/framework/shim/global';
+import { ProjectResourcePathPayload } from '../../processes/interfaces';
 
 export interface ViewProjectPageProperties {
 	loginUser: User;
 	project: Project;
 	resource: ProjectResource; // 项目资源，此处指一个页面
 	groups: ProjectResourceGroup[];
+	onGotoGroup: (opt: ProjectResourcePathPayload) => void;
 }
 
 @theme(css)
@@ -20,7 +23,7 @@ export default class ViewProjectPage extends ThemedMixin(I18nMixin(WidgetBase))<
 	//private _localizedMessages = this.localizeBundle(messageBundle);
 
 	protected render() {
-		const { project, loginUser, resource, groups } = this.properties;
+		const { project, loginUser, resource, groups, onGotoGroup } = this.properties;
 		if (!resource) {
 			return w(Spinner, {});
 		}
@@ -39,6 +42,15 @@ export default class ViewProjectPage extends ThemedMixin(I18nMixin(WidgetBase))<
 				fetchPageModel: `/designer/pages/${pageId}/model`,
 				fetchIdeDependenceInfos: `/designer/projects/${projectId}/dependences?category=ide`,
 				externalScriptAndCssWebsite: '/'
+			},
+			routes: {
+				profile: 'profile',
+				parentGroup: 'view-project',
+				// 当 dojo 路由支持通配符后，删除此段代码
+				gotoGroup: (owner: string, project: string, parentPath: string) => {
+					onGotoGroup({ owner, project, parentPath });
+					global.window.history.pushState({}, '', `/${owner}/${project}/groups/${parentPath}`);
+				}
 			}
 		});
 	}
