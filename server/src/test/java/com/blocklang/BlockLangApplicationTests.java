5e40ea42139278548c2a313ed3393cb7b3fae86c
xiaohulu <idocument@qq.com>
2019-12-16 09:36:10 +0800

    运行 BlockLangApplicationTests 时忽略 BlocklangRunner

--- server/src/test/java/com/blocklang/BlockLangApplicationTests.java
+++ server/src/test/java/com/blocklang/BlockLangApplicationTests.java
@@ -2,10 +2,16 @@ package com.blocklang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.blocklang.listener.BlockLangRunner;
 
 @SpringBootTest
 public class BlockLangApplicationTests {
 
+	@MockBean
+	private BlockLangRunner blocklangRunner;
+	
 	@Test
 	public void contextLoads() {
 	}
