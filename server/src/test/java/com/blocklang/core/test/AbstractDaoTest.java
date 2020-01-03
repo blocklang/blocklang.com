c2246392da031b73dbc00443baf4484960a2369d
xiaohulu <idocument@qq.com>
2020-01-03 17:22:02 +0800

    增加专用于测试 dao 实现类的测试基类

---
+++ server/src/test/java/com/blocklang/core/test/AbstractDaoTest.java
@@ -0,0 +1,28 @@
+package com.blocklang.core.test;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import com.blocklang.BlockLangApplication;
+import com.blocklang.listener.BlockLangRunner;
+
+@SpringBootTest(classes = BlockLangApplication.class)
+@Transactional
+public class AbstractDaoTest extends AbstractSpringTest {
+	
+	// 当运行 service 测试用例时，都会运行 BlockLangRunner，此处改为使用 mock 的 BlockLangRunner。
+	@MockBean
+	private BlockLangRunner blockLangRunner;
+
+	@Autowired
+	protected JdbcTemplate jdbcTemplate;
+
+	protected int countRowsInTable(String tableName) {
+		return JdbcTestUtils.countRowsInTable(jdbcTemplate, tableName);
+	}
+}
