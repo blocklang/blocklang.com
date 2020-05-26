d9e285378355ead662d6a741f1d67661840dbe71
xiaohulu <idocument@qq.com>
2020-05-26 17:08:14 +0800

    feat: 支持校验三类 api 仓库中的 json 格式

---
+++ server/src/test/java/com/blocklang/marketplace/apiparser/WebApiChangeSetSchemaValidatorTest.java
@@ -0,0 +1,53 @@
+package com.blocklang.marketplace.apiparser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.networknt.schema.ValidationMessage;
+
+public class WebApiChangeSetSchemaValidatorTest {
+	@Test
+	public void run_param_is_null() {
+		assertThrows(IllegalArgumentException.class, () -> new WebApiChangeSetSchemaValidator().run(null));
+	}
+	
+	@Test
+	public void run_no_data() {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = mapper.createObjectNode();
+		Set<ValidationMessage> errors = new WebApiChangeSetSchemaValidator().run(rootNode);
+		assertThat(errors).hasSize(3);
+	}
+	
+	@Test
+	public void run_create_object_name_required() {
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode rootNode = mapper.createObjectNode();
+		
+		rootNode.put("id", "id1");
+		rootNode.put("author", "author1");
+		
+		ArrayNode changeNodes = mapper.createArrayNode();
+		ObjectNode change1 = mapper.createObjectNode();
+		ObjectNode createObject = mapper.createObjectNode();
+
+		createObject.put("description", "");
+		change1.set("createObject", createObject);
+		changeNodes.add(change1);
+		
+		rootNode.set("changes", changeNodes);
+		
+		Set<ValidationMessage> errors = new WebApiChangeSetSchemaValidator().run(rootNode);
+		// FIXME: 应该只提示缺少 name，但却也提示缺少 addFunction，这可能是 json validator 库的 bug
+		assertThat(errors).hasSize(2);
+	}
+	
+}
---
+++ server/src/test/java/com/blocklang/marketplace/apiparser/WidgetChangeSetSchemaValidatorTest.java
@@ -0,0 +1,29 @@
+package com.blocklang.marketplace.apiparser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.ValidationMessage;
+
+// schema validator 主要测试 schema 文件中的配置
+public class WidgetChangeSetSchemaValidatorTest {
+
+	@Test
+	public void run_param_is_null() {
+		assertThrows(IllegalArgumentException.class, () -> new WidgetChangeSetSchemaValidator().run(null));
+	}
+	
+	@Test
+	public void run_no_data() {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = mapper.createObjectNode();
+		Set<ValidationMessage> errors = new WidgetChangeSetSchemaValidator().run(rootNode);
+		assertThat(errors).hasSize(3);
+	}
+}
